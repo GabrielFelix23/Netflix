e4d2b383e554f67a1468d25677c9fdd62264721d
GabrielFelix23 <srgfelix123@gmail.com>
2020-11-25 14:15:42 -0300

    Tela Home pronta

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import {Container, ContainerText, ContainerForm} from './style'
 
 function Home(){
     return(
-        <div>
-            <h1>Tela Home</h1>
-        </div>
+        <Container>
+            <ContainerText>
+                <h1>100% de diversão garantida.</h1>
+                <h1>Ou devolvemos o seu dinheiro.</h1>
+                <p>Você vai adorar a Netflix! Mas, se não gostar, cancele nos primeiros 21 dias para receber o reembolso.</p>
+            </ContainerText> 
+            
+            <ContainerForm>
+                <p>Pronto para assistir? Informe seu email para criar ou reiniciar sua assinatura.</p>
+                <form>
+                    <input type="email" placeholder="Email"/>
+                    <button>VAMOS LÁ</button>
+                </form>
+                <p>Esta oferta está disponível somente para não assinantes.</p>
+            </ContainerForm>
+           
+        </Container>
     )
 }
 
---
+++ src/Pages/Home/style.js
@@ -0,0 +1,62 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    height: 120vh;
+`
+export const ContainerText = styled.div`
+    width: 100%;
+    max-width: 640px;
+    text-align: center;
+
+    h1{
+        font-size: 50px;
+    }
+    p{
+        width: 100%;
+        font-size: 25px;
+        margin-bottom: 25px
+    }
+`
+
+export const ContainerForm = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    text-align: center;
+
+    p{
+        width: 100%;
+        font-size: 20px;
+        margin-bottom: 20px
+    }
+
+    form{
+        display: flex;
+        align-items: center;
+        flex-direction: row;
+        width: 100%;
+        margin-bottom: 20px;
+
+        input{
+            width: 100%;
+            height: 8vh;
+            padding: 10px;
+            font-size: 17px;
+        }
+        
+        button{
+            width: 25%;
+            height: 8vh;
+
+            border: none;
+            background: #E50914;
+            color: #fff;
+            font-size: 18px
+        }
+    }
+` 
